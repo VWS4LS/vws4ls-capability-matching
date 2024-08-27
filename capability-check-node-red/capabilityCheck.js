6086a755719794d4b1e91163d21b662672c418ed
Matthias Freund <matthias.freund@festo.com>
2024-08-27 15:53:31 +0200

    Add error handling to the node-red node

--- capability-check-node-red/capabilityCheck.js
+++ capability-check-node-red/capabilityCheck.js
@@ -4,22 +4,34 @@ module.exports = function (RED) {
     function CapabilityCheckNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        node.on('input', async function (msg) {
-            let payload = msg.payload;
+        node.on('input', async function (msg, send, done) {
 
-            let result = await capabilityCheck.executeCapabilityCheck(
-                payload.endpoint,
-                payload.requiredCapabiltySubmodelId,
-                payload.requiredCapabilityContainerIdShortPath,
-                payload.machineAasId,
-                payload.instanceCheck,
-                payload.username,
-                payload.password
-            );
 
-            msg.payload = result;
+            try {
+                let payload = msg.payload;
 
-            node.send(msg);
+
+                let result = await capabilityCheck.executeCapabilityCheck(
+                    payload.endpoint,
+                    payload.requiredCapabiltySubmodelId,
+                    payload.requiredCapabilityContainerIdShortPath,
+                    payload.machineAasId,
+                    payload.instanceCheck,
+                    payload.username,
+                    payload.password
+                );
+
+                msg.payload = result;
+                send(msg);
+                if (done) {
+                    done();
+                }
+            } catch (error) {
+                if (done) {
+                    done(error);
+                }
+            }
+            
         });
     }
     RED.nodes.registerType("capability-check", CapabilityCheckNode);
